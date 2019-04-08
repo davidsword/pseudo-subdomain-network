bc32690b5143ff26ef1f70f17a1ca31a22dec70b
David Sword <david@davidsword.ca>
2019-04-08 16:01:54 -0700

    📦 NEW: init commit of `wp scaffold plugin` content

---
+++ network-pseduo-sub-domains.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     network-pseduo-sub-domains
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     network-pseduo-sub-domains
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         network-pseduo-sub-domains
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package network-pseduo-sub-domains
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
