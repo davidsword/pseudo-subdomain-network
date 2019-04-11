ab05ff9f67beb10922d6643692e99a4bf56ee437
David Sword <david@davidsword.ca>
2019-04-11 08:40:16 -0700

    🐛 FIX: change textdomain in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function( grunt ) {
 
 		addtextdomain: {
 			options: {
-				textdomain: 'network-pseduo-sub-domains',
+				textdomain: 'npsdds',
 			},
 			update_all_domains: {
 				options: {
@@ -32,8 +32,8 @@ module.exports = function( grunt ) {
 				options: {
 					domainPath: '/languages',
 					exclude: [ '\.git/*', 'bin/*', 'node_modules/*', 'tests/*' ],
-					mainFile: 'network-pseduo-sub-domains.php',
-					potFilename: 'network-pseduo-sub-domains.pot',
+					mainFile: 'npsdds.php',
+					potFilename: 'npsdds.pot',
 					potHeaders: {
 						poedit: true,
 						'x-poedit-keywordslist': true
